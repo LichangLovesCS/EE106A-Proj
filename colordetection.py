0dba0ab6da6a55651bb82fc2f6bd3aa3e6106468
LichangLovesCS <xulichang1995@hotmail.com>
2016-12-08 19:01:43 -0800

    real-time image processing added

---
+++ colordetection.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Filename      : colordetection.py
+# Author        : Lichang Xu
+# Created       : November, 2016
+# Last Modified : December, 2016
+
+from sys import exit as Die
+try:
+    import sys
+except ImportError as err:
+    Die(err)
+
+class ColorDetection:
+
+    def get_color_name(self, hsv):
+        """ Get the name of the color based on the hue.
+            Need adjustment for each different color scheme.
+
+        :returns: string
+        """
+        (h,s,v) = hsv
+        #print((h,s,v))
+        if h < 15 and v < 100:
+            return 'red'
+        if h <= 10 and v > 100:
+            return 'orange'
+        elif h <= 30 and s <= 100:
+            return 'white'
+        elif h <= 40:
+            return 'yellow'
+        elif h <= 85:
+            return 'green'
+        elif h <= 130:
+            return 'blue'
+
+        return 'white'
+
+    def name_to_rgb(self, name):
+        """
+        Get the main RGB color for a name.
+
+        :param name: the color name that is requested
+        :returns: tuple
+        """
+        color = {
+            'red'    : (0,0,255),
+            'orange' : (0,165,255),
+            'blue'   : (255,0,0),
+            'green'  : (0,255,0),
+            'white'  : (255,255,255),
+            'yellow' : (0,255,255)
+        }
+        return color[name]
+
+    def average_hsv(self, roi):
+        """ Average the HSV colors in a region of interest.
+
+        :param roi: the image array
+        :returns: tuple
+        """
+        h   = 0
+        s   = 0
+        v   = 0
+        num = 0
+        for y in range(len(roi)):
+            if y % 10 == 0:
+                for x in range(len(roi[y])):
+                    if x % 10 == 0:
+                        chunk = roi[y][x]
+                        num += 1
+                        h += chunk[0]
+                        s += chunk[1]
+                        v += chunk[2]
+        h /= num
+        s /= num
+        v /= num
+        return (int(h), int(s), int(v))
+
+ColorDetector = ColorDetection()
